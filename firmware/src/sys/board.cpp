3742863829aad8fc0a3c86bcb73e9d34cfeacc9a
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-06-06 15:23:11 +0300

    Fixed GPIO config for demoboard

--- firmware/src/sys/board.cpp
+++ firmware/src/sys/board.cpp
@@ -123,7 +123,8 @@ constexpr PinMuxGroup pinmux[] =
     { IOCON_PIO1_6,  IOCON_FUNC0 | IOCON_HYS_EN | IOCON_MODE_PULLUP },                          // DIP_2
     { IOCON_PIO1_7,  IOCON_FUNC1 | IOCON_HYS_EN | IOCON_MODE_PULLUP },                          // UART_TXD
 #if BOARD_OLIMEX_LPC_P11C24
-    { IOCON_PIO1_11, IOCON_FUNC0 },                                                             // CAN LED
+    { IOCON_PIO1_10, IOCON_FUNC0 | IOCON_HYS_EN | IOCON_DIGMODE_EN },                           // LED2
+    { IOCON_PIO1_11, IOCON_FUNC0 | IOCON_HYS_EN | IOCON_DIGMODE_EN },                           // CAN LED
 #endif
     // PIO2
     { IOCON_PIO2_0,  IOCON_FUNC0 | IOCON_HYS_EN | IOCON_MODE_PULLDOWN},                         // Status LED
