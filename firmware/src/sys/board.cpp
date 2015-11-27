d10b7040c33ef8f4fe6f164d7d1a57c10cadde55
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-11-27 18:49:18 +0300

    Fixed unique ID reading

--- firmware/src/sys/board.cpp
+++ firmware/src/sys/board.cpp
@@ -305,10 +305,10 @@ __attribute__((optimize(0)))     // Optimization must be disabled lest it hardfa
 #endif
 void readUniqueID(UniqueID& out_uid)
 {
-    unsigned aligned_array[4] = {};  // out_uid may be unaligned, so we need to use temp array
+    unsigned aligned_array[5] = {};  // out_uid may be unaligned, so we need to use temp array
     unsigned iap_command = 58;
     reinterpret_cast<void(*)(void*, void*)>(0x1FFF1FF1)(&iap_command, aligned_array);
-    std::memcpy(out_uid.data(), aligned_array, 16);
+    std::memcpy(out_uid.data(), &aligned_array[1], 16);
 }
 
 bool tryReadDeviceSignature(DeviceSignature& out_signature)
